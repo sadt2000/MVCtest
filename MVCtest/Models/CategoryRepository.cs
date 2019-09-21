e4bd4465a5873e3a3db2a5619b5ea33d8c7eab54
龍 威 <sadt2000@yahoo.com.tw>
2019-09-21 23:42:03 +0800

    text2

--- MVCtest/Models/CategoryRepository.cs
+++ MVCtest/Models/CategoryRepository.cs
@@ -5,8 +5,32 @@ using System.Web;
 
 namespace MVCtest.Models
 {
-    public class CategoryRepository
+    public class CategoryRepository : ICategoryRepository
     {
+        private NorthwindEntities db = new NorthwindEntities();
+        public void Creat(Categories categories)
+        {
+            throw new NotImplementedException();
+        }
 
+        public void Delete(Categories categories)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Categories> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Categories GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Categories categories)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
